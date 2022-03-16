849eeab14ff99885dd52428baa2da383e01f1c83
Марат Дьяченко <maratka0708@yandex.ru>
2022-03-16 22:17:20 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,22 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2)-4*a*c;
+  if (d === 0) {
+    arr.push(-b/(2*a));
+  }
+  else if (d > 0) {
+    arr.push((-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d))/(2*a));
+  }
+  else {
+    arr.push();
+  }
+  
+  return arr;
 }
+
+solveEquation();
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
